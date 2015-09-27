6753a32d355f15a3eeff3887ed5530673f738f38
Aaron Myers <subdriven@gmail.com>
2015-09-27 17:38:42 -0400

    lol

--- intro_to_flask/routes.py
+++ intro_to_flask/routes.py
@@ -33,7 +33,7 @@ def contact():
   elif request.method == 'GET':
     return render_template('contact.html', form=form)
  
-@app.route('/signup2', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
   form = SignupForm()
    
