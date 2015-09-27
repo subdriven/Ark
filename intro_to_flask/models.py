688189582e4b79efc0dd123d7a22a0ec352e29e5
Aaron Myers <subdriven@gmail.com>
2015-09-27 15:53:46 -0400

    Indent fixes

--- intro_to_flask/models.py
+++ intro_to_flask/models.py
@@ -11,14 +11,14 @@ class Donkey(db.Model):
   	email = db.Column(db.String(120), unique=True)
   	pwdhash = db.Column(db.String(54))
    
-  def __init__(self, firstname, lastname, email, password):
-    self.firstname = firstname.title()
-    self.lastname = lastname.title()
-    self.email = email.lower()
-    self.set_password(password)
+	def __init__(self, firstname, lastname, email, password):
+	    self.firstname = firstname.title()
+	    self.lastname = lastname.title()
+	    self.email = email.lower()
+	    self.set_password(password)
      
-  def set_password(self, password):
-    self.pwdhash = generate_password_hash(password)
+	def set_password(self, password):
+    	self.pwdhash = generate_password_hash(password)
    
-  def check_password(self, password):
-    return check_password_hash(self.pwdhash, password)
+	def check_password(self, password):
+    	return check_password_hash(self.pwdhash, password)
