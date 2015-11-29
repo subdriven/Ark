6087f9d2e8a7462ff3995e45eab3c0588c11acfc
Aaron <subdriven@gmail.com>
2015-11-29 12:32:03 -0500

    Trying to merge 2 demos into 1

--- intro_to_flask/routes.py
+++ intro_to_flask/routes.py
@@ -8,11 +8,15 @@ mail = Mail()
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return render_template('home.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/search')
+def search():
+  return render_template('search.html')
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
